47e8dd398909f1402b53bc873b7c2866122c5bd6
bbzbryce <bbzbryce@localhost>
2008-10-20 21:31:54 +0000

    Added setup.py so that the package can easily be installed.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CRAWL-E',
+      version='.1',
+      description='Highly distributed web crawling framework',
+      author='Bryce Boe',
+      author_email='bboe (_at_) cs.ucsb.edu',
+      url='http://code.google.com/p/crawl-e',
+      packages=['Crawle']
+      )
